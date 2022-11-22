2081d8bd88a1307e9428c8db635287d0c416d3af
dario-ferrari <57733048+dario-ferrari@users.noreply.github.com>
2022-11-22 03:01:01 -0300

    Add files via upload

---
+++ frontend/src/components/client/client.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './components/header'
+
+function Client() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
+}
+
+export default Client
